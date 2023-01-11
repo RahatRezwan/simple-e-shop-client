0beb03d6c21a3c45df3cbfe5d8a0ae2f82dd8e06
Kazi Rahat <kazirahat1007@outlook.com>
2023-01-11 15:15:04 +0600

    export to excel functionality added

--- src/pages/AllUsers/AllUsers.jsx
+++ src/pages/AllUsers/AllUsers.jsx
@@ -9,6 +9,8 @@ import SmallSpinner from "../../components/SmallSpinner/SmallSpinner";
 import UpdateModal from "../../components/UpdateModal/UpdateModal";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
+import { utils, write } from "xlsx";
+import FileSaver from "file-saver";
 
 const AllUsers = () => {
    const [currentUser, setCurrentUser] = useState(null);
@@ -46,12 +48,13 @@ const AllUsers = () => {
       doc.save("userData.pdf");
    }
 
-   /* const exportToExcel = () => {
-      const ws = XLSX.utils.json_to_sheet(userData);
-      const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-      XLSX.writeFile(wb, "User.xlsx");
-   }; */
+   const exportToExcel = () => {
+      const ws = utils.json_to_sheet(userData);
+      const wb = utils.book_new();
+      utils.book_append_sheet(wb, ws, "Sheet1");
+      const wbout = write(wb, { bookType: "xlsx", type: "array" });
+      FileSaver.saveAs(new Blob([wbout], { type: "application/octet-stream" }), "userData.xlsx");
+   };
 
    const [hiddenColumns, setHiddenColumns] = useState([]);
    const headers = ["User", "Email", "Role", "Plan", "Status"];
@@ -87,7 +90,10 @@ const AllUsers = () => {
                      <img src={exportIcon} alt="" />
                      PDF
                   </button>
-                  <button className="flex gap-2 items-center py-[7px] px-[22px] border rounded-md hover:shadow-md">
+                  <button
+                     onClick={exportToExcel}
+                     className="flex gap-2 items-center py-[7px] px-[22px] border rounded-md hover:shadow-md"
+                  >
                      <img src={exportIcon} alt="" />
                      EXCEL
                   </button>
